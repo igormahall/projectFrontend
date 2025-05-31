da20b356a5339ea165f2604892088b0f3d20df0e
igormahall <igormahall@gmail.com>
2025-05-30 20:17:16 -0400

    Aula 30/05 - atualizado ngOnInit

--- src/app/components/clima/clima.component.ts
+++ src/app/components/clima/clima.component.ts
@@ -14,4 +14,23 @@ export class ClimaComponent {
   climaData: any;
   date: Date = new Date();
   dataTable: ClimaTable[]=[];
+
+  subtrair4horas(dataEmString:string) {
+    let novaData = new Date(dataEmString);
+    novaData.setHours(novaData.getHours()-4);
+    return novaData;
+  }
+
+  ngOnInit() {
+    this.climaService.getClima().subscribe((response:any) => {
+      this.climaData = response;
+      this.date = this.subtrair4horas(response.current.time);
+      this.dataTable = response.hourly.time.map((t:string, i:number) => ({
+        time: this.subtrair4horas(t),
+        temperature: response.hourly.temperature_2m[i],
+        humidity: response.hourly.relative_humidity_2m[i],
+        windSpeed: response.hourly.wind_speed_10m[i]
+      }));
+    });
+  }
 }
